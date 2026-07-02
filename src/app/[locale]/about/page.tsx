f5fe9dd65aba9f568c844c8dfc239319bc55fbd4
Bjorn Brassé <bpbrasse@bra-c.nl>
2026-07-02 20:24:38 +0200

    improved text about us

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,4 +1,4 @@
-import { EarthIcon } from "lucide-react";
+import { EarthIcon, HandshakeIcon, KeyRoundIcon, TabletsIcon } from "lucide-react";
 import type { Metadata } from "next";
 import { getTranslations, setRequestLocale } from "next-intl/server";
 
@@ -22,59 +22,22 @@ const teamMembers = [
 const values = [
   {
     key: "education" as const,
-    icon: (
-      <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M12 14l9-5-9-5-9 5 9 5zm0 7l-9-5 9-5 9 5-9 5z"
-        />
-      </svg>
-    ),
+    icon: <TabletsIcon />,
     accent: "text-ghana-green bg-ghana-green/10",
   },
   {
     key: "sustainability" as const,
     icon: <EarthIcon />,
-    // icon: (
-    //   <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-    //     <path
-    //       strokeLinecap="round"
-    //       strokeLinejoin="round"
-    //       strokeWidth={2}
-    //       d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0z"
-    //     />
-    //   </svg>
-    // ),
     accent: "text-yellow-600 bg-ghana-gold/20",
   },
   {
-    key: "integrity" as const,
-    icon: (
-      <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
-        />
-      </svg>
-    ),
+    key: "accessibility" as const,
+    icon: <KeyRoundIcon />,
     accent: "text-blue-600 bg-blue-50",
   },
   {
-    key: "excellence" as const,
-    icon: (
-      <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-        />
-      </svg>
-    ),
+    key: "partnership" as const,
+    icon: <HandshakeIcon />,
     accent: "text-ghana-red bg-ghana-red/10",
   },
 ];
